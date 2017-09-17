a832a535d56f492dff8f7101e03343593c6513fd
yetiancai <yetiancai@bigo.sg>
2017-09-17 20:26:39 +0800

    # sync model dir

--- windows/rsync_workspace.sh
+++ windows/rsync_workspace.sh
@@ -2,5 +2,5 @@
 rsync -e "ssh -i /cygdrive/c/Users/Administrator/Documents/SSH_Keys/IdentityOpenSSH" \
 -avzP \
 --exclude ".vscode" --exclude "*__pycache__*" \
---exclude "*/model" --exclude ".idea" \
+--exclude ".idea" \
 /cygdrive/e/workspace yetiancai@gz-gpu005.cvmarcher.cn:/home/yetiancai/
