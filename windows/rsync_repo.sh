d43d2912a841cc7e88eb4401c09904627fe1f596
yetiancai <yetiancai@bigo.sg>
2017-09-17 19:53:34 +0800

    # removee delete

--- windows/rsync_repo.sh
+++ windows/rsync_repo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rsync -e "ssh -i /cygdrive/c/Users/Administrator/Documents/SSH_Keys/IdentityOpenSSH" \
--avzP --delete \
+-avzP \
 /cygdrive/e/repo yetiancai@gz-gpu005.cvmarcher.cn:/home/yetiancai/
