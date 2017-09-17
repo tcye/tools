af2445a830303ff78fa9f587484482ef9f536ebf
yetiancai <yetiancai@bigo.sg>
2017-09-17 16:40:04 +0800

    # vim

--- rsync_gpu_servers.sh
+++ rsync_gpu_servers.sh
@@ -5,5 +5,7 @@ for i in ${gpu_servers[@]}; do
     rsync -avP --delete ~/rec gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/models gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/anaconda3 gz-gpu00${i}.cvmarcher.cn:~
+    rsync -avP --delete ~/.vimrc gz-gpu00${i}.cvmarcher.cn:~
+    rsync -avP --delete ~/.vim gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/workspace gz-gpu00${i}.cvmarcher.cn:~
 done
