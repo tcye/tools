69344b85a20813d9721ec13e256c81fabbcd1a62
Tiancai Ye <yetiancai@bigo.sg>
2017-10-12 17:35:32 +0800

    # update anaconda's position

--- rsync_gpu_servers.sh
+++ rsync_gpu_servers.sh
@@ -4,7 +4,7 @@ for i in ${gpu_servers[@]}; do
     rsync -avP --delete ~/repo gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/rec gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/models gz-gpu00${i}.cvmarcher.cn:~
-    rsync -avP --delete ~/anaconda3 gz-gpu00${i}.cvmarcher.cn:~
+    rsync -avP --delete ~/.local gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/.vimrc gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/.vim gz-gpu00${i}.cvmarcher.cn:~
     rsync -avP --delete ~/.condarc gz-gpu00${i}.cvmarcher.cn:~
