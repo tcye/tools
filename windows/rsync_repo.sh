8da552e432829d62c8b15e801278f487bc293454
Tiancai Ye <yetiancai@bigo.sg>
2017-10-12 17:33:29 +0800

    # update exclude

--- windows/rsync_repo.sh
+++ windows/rsync_repo.sh
@@ -5,5 +5,10 @@ rsync -e "ssh -i /cygdrive/c/Users/Administrator/Documents/SSH_Keys/IdentityOpen
 --exclude .idea \
 --exclude .vscode \
 --exclude __pycache__ \
---exclude libmxnet.so \
+--exclude mxnet_tcye/**.so \
+--exclude mxnet_tcye/**.a \
+--exclude mxnet_tcye/**.o \
+--exclude mxnet_tcye/bin \
+--exclude build/ \
+--exclude lib/ \
 /cygdrive/e/repo yetiancai@gz-gpu005.cvmarcher.cn:/home/yetiancai/
