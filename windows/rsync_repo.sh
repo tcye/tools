aa4375f3cab84dc2b15f6d47e2313774edcdc1dd
Tiancai Ye <yetiancai@bigo.sg>
2017-10-12 15:49:30 +0800

    # add some exclude pattern

--- windows/rsync_repo.sh
+++ windows/rsync_repo.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 rsync -e "ssh -i /cygdrive/c/Users/Administrator/Documents/SSH_Keys/IdentityOpenSSH" \
--avzP \
+-avzP --delete \
+--exclude .vs \
+--exclude .idea \
+--exclude .vscode \
+--exclude __pycache__ \
+--exclude libmxnet.so \
 /cygdrive/e/repo yetiancai@gz-gpu005.cvmarcher.cn:/home/yetiancai/
--- windows/rsync_workspace.sh
+++ windows/rsync_workspace.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 rsync -e "ssh -i /cygdrive/c/Users/Administrator/Documents/SSH_Keys/IdentityOpenSSH" \
--avzP \
---exclude ".vscode" --exclude "*__pycache__*" \
---exclude ".idea" \
+-avzP --delete \
+--exclude .vs \
+--exclude .idea \
+--exclude .vscode \
+--exclude __pycache__ \
+--exclude model-symbol.json \
+--exclude model-*.params \
+--exclude train.log \
+--exclude img_list \
+--exclude ann_map \
 /cygdrive/e/workspace yetiancai@gz-gpu005.cvmarcher.cn:/home/yetiancai/
