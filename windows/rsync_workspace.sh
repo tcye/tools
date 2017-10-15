1a41885d17de1e545399c68c9b51d405499f62c8
Tiancai Ye <yetiancai@bigo.sg>
2017-10-15 22:22:48 +0800

    # update

--- windows/rsync_workspace.sh
+++ windows/rsync_workspace.sh
@@ -5,10 +5,14 @@ rsync -e "ssh -i /cygdrive/c/Users/Administrator/Documents/SSH_Keys/IdentityOpen
 --exclude .idea \
 --exclude .vscode \
 --exclude __pycache__ \
---exclude model-symbol.json \
---exclude model-*.params \
+--exclude *.json \
+--exclude *.params \
 --exclude train.log \
 --exclude img_list \
 --exclude ann_map \
 --exclude *.npy \
+--exclude VOCdevkit \
+--exclude *.rec \
+--exclude *.lst \
+--exclude *.idx \
 /cygdrive/e/workspace yetiancai@gz-gpu005.cvmarcher.cn:/home/yetiancai/
